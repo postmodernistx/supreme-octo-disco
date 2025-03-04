bee36df4f58cf9196d7d81c3d41109f62131c58d
Random Nickname 123456789 <jenni@ohsomodern.org>
2025-03-04 16:14:40 +0200

    add header

---
+++ src/fixtures/header/MainHeader.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { ref } from 'vue';
+
+import MenuButton from '@/components/atoms/MenuButton.vue';
+
+const showNav = ref(false);
+
+function toggleNav() {
+  showNav.value = !showNav.value;
+}
+</script>
+
+<template>
+  <header>
+    <MenuButton class="toggle-menu" @change="toggleNav" />
+
+    <h1>Landing page</h1>
+
+    <button class="buy-now">Buy now</button>
+
+    <p>Menyn öppen: {{ showNav }}</p>
+
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+@import 'header.scss';
+</style>
