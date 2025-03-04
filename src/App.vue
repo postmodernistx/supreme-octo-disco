1d8ec975fbb19b69902a781eda54a6e49dcbdcd4
Random Nickname 123456789 <jenni@ohsomodern.org>
2025-03-04 17:52:14 +0200

    ✨ feat: add icon button component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
 import MainHeader from '@/fixtures/header/MainHeader.vue';
+import IconButton from '@/components/atoms/IconButton.vue'
 </script>
 
 <template>
   <MainHeader />
+
+  <IconButton button-text="Köp nu!" />
+  <IconButton button-text="Köp genast!" icon="🛒" />
+
   <!-- <RouterView /> -->
   <!-- <MainFooter />-->
 </template>
---
+++ src/components/atoms/IconButton.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+
+const props = defineProps({
+  buttonText: { type: String, required: true },
+  icon: { type: String, required: false },
+});
+
+onMounted(() => {
+  if (props.icon) {
+    console.log('Denna knapp har en ikon:', props.buttonText);
+  }
+});
+</script>
+
+<template>
+  <button :class="{ 'has-icon': icon }">
+    <span v-if="icon">{{ icon }}</span>
+    {{ buttonText }}
+  </button>
+</template>
+
+<style scoped lang="scss">
+button {
+  padding: 8px 16px;
+  background: #2c3e50;
+  color: #fff;
+  border: 0;
+  border-radius: 8px;
+  box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.5);
+  cursor: pointer;
+  transition: background 0.3s cubic-bezier(0.785, 0.135, 0.15, 0.86);
+
+  &:hover {
+    background: dodgerblue;
+  }
+
+  &.has-icon {
+    background-color: antiquewhite;
+    color: black;
+  }
+}
+
+span {
+  display: inline-block;
+  margin-right: 4px;
+}
+</style>
